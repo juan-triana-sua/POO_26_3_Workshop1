1c4e1acf495ef1d7fde96757fd98371af36d12ff
juan triana sua <juancuby15@gmeil.com>
2026-09-13 18:46:16 -0500

    ejercicio numeroPrimo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -52,7 +52,18 @@ public class Workshop {
     public boolean esPrimo(int numero) {
         // TODO: Implementar el método para verificar si un número es primo.
         // Ejemplo: Si numero = 7, el resultado debería ser true.
+
+	 if (numero < 2) {
         return false;
+    }
+
+    for (int i=2; i<numero; i++) {
+        if (numero % i==0) {
+            return false;
+        }
+    }
+
+    return true;
     }
 
     // Método que genera una serie de Fibonacci
