f0b83b5fb634f1c77c99c94e0263746d292e3156
juan triana sua <juancuby15@gmeil.com>
2026-09-13 19:32:18 -0500

    ejercicio busquedaNumeroMayor

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -124,7 +124,16 @@ public class Workshop {
     public int encontrarElementoMayor(int[] arreglo) {
         // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
-        return 0;
+    
+	     int mayor = arreglo[0];
+
+    for(int a=1; a<arreglo.length; a++){
+        if(arreglo[a] > mayor){
+            mayor = arreglo[a];
+        }
+    }
+
+    return mayor;
     }
 
     // Método que encuentra el elemento menor en un arreglo
