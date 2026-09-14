dba053a2f5ed26e883156e1cc42279f266e196a7
juan triana sua <juancuby15@gmeil.com>
2026-09-13 19:35:47 -0500

    ejercicio busquedaNumeroMenor

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -140,7 +140,16 @@ public class Workshop {
     public int encontrarElementoMenor(int[] arreglo) {
         // TODO: Implementar el método para encontrar el elemento menor en un arreglo.
         // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 1.
-        return 0;
+        
+	    int menor = arreglo[0];
+
+    for(int a=1; a<arreglo.length; a++){
+        if(arreglo[a] < menor){
+            menor = arreglo[a];
+        }
+    }
+
+    return menor;
     }
 
     // Método que busca un elemento en un arreglo
