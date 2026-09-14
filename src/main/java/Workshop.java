13fe08c813eab3cfe8ff27850bc9d67d8c598f73
juan triana sua <juancuby15@gmeil.com>
2026-09-13 22:53:00 -0500

    ejercicio contadorDePalabras

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -312,7 +312,14 @@ public class Workshop {
     public int contarPalabras(String cadena) {
         // TODO: Implementar el método para contar el número de palabras en una cadena.
         // Ejemplo: Si cadena = "Este es un test", el resultado debería ser 4.
+   
+    if(cadena.trim().isEmpty()){
         return 0;
+    }
+
+    String[] palabras = cadena.trim().split("\\s+");
+
+    return palabras.length;
     }
 
     // Método que convierte una cadena a mayúsculas
