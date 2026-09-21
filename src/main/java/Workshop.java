3af0c2fc0d63f31c6ae834c61760e507d616b3e2
juan triana sua <juancuby15@gmeil.com>
2026-09-20 21:21:41 -0500

    ejercicio de area de circulo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -551,7 +551,8 @@ Rock crushes Scissors
     }
 
     public double areaCirculo(double radio) {
-        return 0.0;
+
+	return Math.PI * radio * radio;
     }
 
     public String zoodiac(int day, int month) {
