3cff40126cfa191bb3164d169338e917a27788a0
juan triana sua <juancuby15@gmeil.com>
2026-09-20 21:52:25 -0500

    correccion de zodiaco *sigo con sueño profe :c*

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -557,12 +557,30 @@ Rock crushes Scissors
 
     public String zoodiac(int day, int month) {
  
- 	   if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
+	 if (month < 1 || month > 12) {
+        return "Invalid Date";
+    }
+
+    int diasDelMes;
+
+    if (month == 2) {
+        diasDelMes = 28;
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        diasDelMes = 30;
+    } else {
+        diasDelMes = 31;
+    }
+
+    if (day < 1 || day > diasDelMes) {
+        return "Invalid Date";
+    }
+
+    if ((month == 3 && day >= 21) || (month == 4 && day <= 19)) {
         return "Aries";
     } else if ((month == 4 && day >= 20) || (month == 5 && day <= 20)) {
         return "Tauro";
     } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
-        return "Geminis";
+        return "Gemini";
     } else if ((month == 6 && day >= 21) || (month == 7 && day <= 22)) {
         return "Cancer";
     } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
@@ -572,15 +590,15 @@ Rock crushes Scissors
     } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
         return "Libra";
     } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
-        return "Escorpio";
+        return "Scorpio";
     } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
-        return "Sagitario";
+        return "Sagittarius";
     } else if ((month == 12 && day >= 22) || (month == 1 && day <= 19)) {
-        return "Capricornio";
+        return "Capricorn";
     } else if ((month == 1 && day >= 20) || (month == 2 && day <= 18)) {
-        return "Acuario";
+        return "Aquarius";
     } else {
-        return "Piscis";
+        return "Pisces";
     }
     }
 
